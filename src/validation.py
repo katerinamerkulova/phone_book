32ed0ee1250efdc9b5a479fe41eed008299058b4
AlexeyMalafeev <amalafeev@yandex.ru>
2020-11-26 12:23:46 +0300

    fix formatting issues in validation.py

--- src/validation.py
+++ src/validation.py
@@ -1,37 +1,46 @@
-'''
+"""
 Docstring
-'''
+"""
 
-from datetime import date
 import re
 
 from phone_book import Birthday
+
 
 def input_name():
     name = input('Please, input attributes of the person: \n name (e.g. Lena) \n')
     is_name_correct = re.match(r'[A-Za-z\d\s]+', name)
     while not is_name_correct:
-        name = input('The name should consist only latin characters, digits or white spaces, first letter must be capital  e.g. Lena \n')
+        name = input('The name should consist only latin characters, digits or white spaces, first '
+                     'letter must be capital  e.g. Lena \n')
         is_name_correct = re.match(r'[A-Za-z\d\s]+', name)
     return name.capitalize()
+
 
 def input_surname():
     surname = input('surname (e.g. Osipova) \n')
     is_surname_correct = re.match(r'[A-Za-z\d\s]+', surname)
     while not is_surname_correct:
-        surname = input('The surname should consist only latin characters, digits or white spaces, first letter must be capital  e.g. Osipova \n')
+        surname = input('The surname should consist only latin characters, digits or white spaces, '
+                        'first letter must be capital  e.g. Osipova \n')
         is_surname_correct = re.match(r'[A-Za-z\d\s]+', surname)
     return surname.capitalize()
+
 
 def input_birthday(year=False):
     if year:
         birthday = input('Input birthday in next format DD/MM \n') + '/1000'
     else:
-        birthday = input('If you want to add birthday, please input it (e.g. 31/01/1999), else press Enter \n')
+        birthday = input('If you want to add birthday, please input it (e.g. 31/01/1999), else '
+                         'press Enter \n')
 
     if birthday:
         is_birthday_correct = re.match(r'\d{2}\/\d{2}\/\d{4}', birthday)
-        birthday = Birthday(birthday)
+        if is_birthday_correct:
+            birthday = Birthday(birthday)
+        else:
+            # try again
+            pass
 
         try:
             if birthday.today <= birthday.birth_date:
@@ -43,10 +52,9 @@ def input_birthday(year=False):
         except ValueError as error:
             print(error)
             is_birthday_correct = False
-        
+
         while not is_birthday_correct:
             birthday = Birthday(input('The birthday should be in next format DD/MM/YYYY \n'))
-            is_birthday_correct = re.match(r'\d{2}\/\d{2}\/\d{4}', birthday)
 
             try:
                 if birthday.today <= birthday.birth_date:
@@ -60,6 +68,7 @@ def input_birthday(year=False):
                 is_birthday_correct = False
     return birthday
 
+
 def input_number():
     number = input('number (e.g. 89245548798) \n')
     print(1, number)
@@ -71,6 +80,7 @@ def input_number():
         is_number_correct = re.match(r'\d{11}', number)
     return number
 
+
 def input_to_find():
     name, surname, birthday, number = None, None, None, None
     num = input('How many attributes to input? (e.g. 2) \n')
@@ -79,7 +89,9 @@ def input_to_find():
         num = input('You have inputed not a number. Please, input the number between 1 and 4 \n')
 
     for i in range(int(num)):
-        input_attr = input('Input the command relevant attribute: \n n - Name \n s - Surname \n b - Birthday \n nb - Number \n')
+        input_attr = input(
+            'Input the command relevant attribute: \n n - Name \n s - Surname \n b - Birthday \n '
+            'nb - Number \n')
 
         if input_attr == 'n':
             name = input_name()
@@ -94,4 +106,3 @@ def input_to_find():
             number = input_number()
 
     return name, surname, birthday, number
-
